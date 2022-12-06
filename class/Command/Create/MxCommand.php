15013300d98bcad56c96afe395576ddadca025ee
André Ricardo <contatoadmx@gmail.com>
2022-12-06 01:44:30 -0300

    commands

---
+++ class/Command/Create/MxCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Command\Create;
+
+use Elegance\File;
+use Elegance\Import;
+use Elegance\MxCmd;
+use Exception;
+
+abstract class MxCommand extends MxCmd
+{
+    protected static function execute($commandName = null)
+    {
+        if (!$commandName)
+            throw new Exception("Informe o nome do comando");
+
+        $tmp = $commandName;
+        $tmp = explode('.', $tmp);
+        $tmp = array_map(fn ($value) => ucfirst($value), $tmp);
+
+        $class = "Mx" . array_pop($tmp);
+
+        $namespace = implode('\\', $tmp);
+        $namespace = trim("Command\\$namespace", '\\');
+
+        $filePath = path(
+            env('PATH_CLASS'),
+            str_replace('\\', '/', $namespace),
+            "$class.php"
+        );
+
+        if (File::check($filePath))
+            throw new Exception("Arquivo [$filePath] já existe");
+
+        $prepare = [
+            '[#]',
+            'name' => $commandName,
+            'class' => $class,
+            'namespace' => $namespace,
+            'PHP' => '<?php'
+        ];
+
+        $base = path(dirname(__DIR__, 3) . '/library/template/mx-create-command.txt');
+
+        $content = Import::content($base, $prepare);
+
+        File::create($filePath, $content);
+
+        MxCmd::show('Comando [[#]] criado com sucesso.', $commandName);
+    }
+}
---
+++ class/Command/MxComposer.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Command;
+
+use Elegance\Dir;
+use Elegance\MxCmd;
+
+abstract class MxComposer extends MxCmd
+{
+    protected static function execute()
+    {
+        $composer = jsonFile('composer');
+
+        $composer['autoload'] = $composer['autoload'] ?? [];
+        $composer['autoload']['psr-4'] = $composer['autoload']['psr-4'] ?? [];
+        $composer['autoload']['psr-4'][''] = path(env('PATH_CLASS') . '/');
+
+        $autoImport = path(env('PATH_HELPER'));
+
+        $files = [];
+
+        foreach ($composer['autoload']['files'] ?? [] as $file)
+            if (substr($file, 0, strlen($autoImport)) != $autoImport)
+                $files[] = $file;
+
+        $files = [...$files, ...self::seek_for_file($autoImport)];
+
+        $composer['autoload']['files'] = $files;
+
+        jsonFile('composer', $composer, false);
+
+        MxCmd::show('Arquivo [composer.json] atualizado');
+
+        self::update();
+    }
+
+    protected static function update()
+    {
+        MxCmd::show('------------------------------------------------------------');
+        echo shell_exec("composer update");
+        MxCmd::show('------------------------------------------------------------');
+    }
+
+    protected static function seek_for_file($ref)
+    {
+        $return = [];
+
+        foreach (Dir::seek_for_dir($ref) as $dir)
+            foreach (self::seek_for_file("$ref/$dir") as $file)
+                $return[] = path($file);
+
+        foreach (Dir::seek_for_file($ref) as $file)
+            $return[] = path("$ref/$file");
+
+        return $return;
+    }
+}
